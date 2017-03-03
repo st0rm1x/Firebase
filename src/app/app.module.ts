115fc22ef8f2578b4c4af4f2812054b88d03a322
Kelvin Wu <wu.kelvin91@gmail.com>
2017-03-03 11:26:32 +0800

    Init Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB4W6IHYY9UYncaZyh3zPRtWWLXwBv2idY",
+  authDomain: "pepper-a241a.firebaseapp.com",
+  databaseURL: "https://pepper-a241a.firebaseio.com",
+  storageBucket: "pepper-a241a.appspot.com",
+  messagingSenderId: "95483207455"
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
