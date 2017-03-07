c37efafae67c660c3575239b279f5a8d78997481
Kelvin Wu <kelvin.wu@bertelsmann.de>
2017-03-07 16:35:53 +0800

    2-17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFire } from 'angularfire2';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'app works!';
-  cuisines;
-  private subscription;
+export class AppComponent {
+  cuisines: FirebaseListObservable<any[]>;
+  restaurants: Observable<any[]>;
 
   constructor(private af: AngularFire) {
-
   }
 
   ngOnInit() {
     this.cuisines = this.af.database.list('/cuisines');
+    this.restaurants = this.af.database.list('/restaurants')
+      .map(restaurants => {
+        restaurants.map(restaurant => {
+          restaurant.featureTypes = [];
+          for (var f in restaurant.features)
+            restaurant.featureTypes.push(this.af.database.object('/features/' + f));
+        });
+        return restaurants;
+      });
   }
 }
